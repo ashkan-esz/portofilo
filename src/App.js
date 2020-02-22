70dbc95d0587a924cf14a882e07d8130fc167365
ashkan-esz <ashkan-esz@users.noreply.github.com>
2020-02-22 22:45:03 +0330

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, {Suspense} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import 'semantic-ui-offline/semantic.min.css'
-import NavBarTitle from './components/NavBar/NavBarTitle';
 
 const NavBar = React.lazy(() => import("./components/NavBar/NavBar"));
 const Home = React.lazy(() => import( "./pages/Home"));
 const Rezome = React.lazy(() => import("./pages/Rezome"));
 const PageNotFound = React.lazy(() => import("./pages/PageNotFound"));
 
-// todo : try to comment font import in css file to see it still get the fonts or not // looks cant do it!!
+// todo : try to comment font import in css file to see it still get the fonts or not // looks cant do that!!
 // todo : why it takes more time to load on mobiles
 // todo : navbar in mobile mode should change
 // todo : navbar in tablet mode should be handle
 // todo : error boundaries
 // todo : show some thing while page is loading
 // todo : use route based splitting to increase performance
+// todo : turn on service worker
 
 function App() {
     return (
         <div>
-            <Suspense fallback={<NavBarTitle/>}>
+            <Suspense fallback={<h1>loading navbar....</h1>}>
                 <NavBar/>
             </Suspense>
 
